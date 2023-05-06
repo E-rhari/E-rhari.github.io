9cd45eab10458a92b4fe1dd3d660ba86a1649bee
Evandro Rhari <93932260+E-rhari@users.noreply.github.com>
2023-05-05 21:08:36 -0300

    groundhog_day

---
+++ pc_web/Aulas/dia_da_marmota/marmota_script.js
@@ -0,0 +1,43 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+const game = document.querySelector(".game");
+let clickActive = false;
+
+
+const selecionaBuraco = () => Math.floor(Math.random() * 6);
+
+const tiraMarmota = (numeroBuraco) =>{
+    const buraco = holes[numeroBuraco];
+    buraco.classList.remove("up");
+}
+
+const brotaMarmota = (numeroBuraco) =>{
+    const buraco = holes[numeroBuraco];
+    buraco.classList.add("up");
+    clickActive = false;
+}
+
+const marmotation = (numeroBuraco) => {
+    brotaMarmota(numeroBuraco);
+    setTimeout(()=>{tiraMarmota(numeroBuraco)}, 1000);
+}
+
+function startGame(){
+    let gameTimer = 300;
+    let intervalo = setInterval(()=>{
+        const num = selecionaBuraco();
+        marmotation(num);;}, gameTimer)
+    
+    setTimeout(() => {clearInterval(intervalo)}, 16000)
+}
+
+game.addEventListener("click", (evento)=>{
+    if('mole' == evento.target.classList[0])
+        if(!clickActive){
+        evento.target.parentNode.classList.remove("up");
+        scoreBoard.innerHTML = parseInt(scoreBoard.innerHTML) + 1;
+        clickActive = true;
+    }
+})
+
